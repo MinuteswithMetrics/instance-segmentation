0727ada11ba1b4f3729b608c0253315882346c72
ykamikawa <ytkmkw@gmail.com>
2018-03-09 14:16:34 +0900

    change train config

--- config.py
+++ config.py
@@ -175,6 +175,10 @@ class DSBConfig(Config):
     # anchor side in pixels
     RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)
 
+    # Ratios of anchors at each cell (width/height)
+    # A value of 1 represents a square anchor, and 0.5 is a wide anchor
+    RPN_ANCHOR_RATIOS = [0.25, 0.5, 1, 2, 4]
+
     # Reduce training ROIs per image because the images are small and have
     # few objects. Aim to allow ROI sampling to pick 33% positive ROIs.
     TRAIN_ROIS_PER_IMAGE = 200
