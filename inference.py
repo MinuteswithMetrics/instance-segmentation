8c3ad074e59c58057c5df235d1198b5cf2eec8c7
ykamikawa <ytkmkw@gmail.com>
2018-03-19 20:59:44 +0900

    change train dataset

--- inference.py
+++ inference.py
@@ -21,7 +21,7 @@ model = modellib.MaskRCNN(mode="inference",
 # Either set a specific path or find last trained weights
 # model_path = os.path.join(ROOT_DIR, ".h5 file name here")
 #model_path = model.find_last()[1]
-model_path = "./logs/16-15-DSB.h5"
+model_path = "./logs/dsb20180316T1622/mask_rcnn_dsb_0034.h5"
 
 # Load trained weights (fill in path to trained weights here)
 assert model_path != "", "Provide path to trained weights"
